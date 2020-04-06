12aee992ca01b66401ba9d9bc76148f8159586a8
DavidN <davidnobles95@gmail.com>
2020-04-06 10:25:02 -0700

    fixed screen update, it was an error with the SnakeBody.at() method

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.red,
       ),
-      home: HomePage(),
+      home: new HomePage(),
     );
   }
 }
